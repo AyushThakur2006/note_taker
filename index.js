94655ca501259ebe0168372615722e7d88e13192
Ayush Thakur <workayush2006.com>
2025-07-20 22:16:32 +0530

    almost

--- index.js
+++ index.js
@@ -6,19 +6,40 @@ app.set("view engine", "ejs");
 app.use(express.json( ));
 app.use(express.urlencoded({extended:true}));
 app.use(express.static(path.join(__dirname,"public")))
+//
 app.get('/',function(req, res)
 {
-//   res.render("index")
   fs.readdir(`./files`,function(err,files){
 res.render("index",{files:files}) 
   })
 })
-app.post('/create',function(req, res)
+//
+app.get('/files/:filename',function(req, res)
+{
+fs.readFile(`./files/${req.params.filename}`,'utf-8',function(err,filedata){
+res.render("show",{filename: req.params.filename ,filedata:filedata})})
+})
+//
+app.get('/edit/:filename',function(req, res)
+{
+res.render("edit", {filename: req.params.filename});
+})
+//
+ app.post('/edit',function(req, res)
 {
     fs.writeFile(`./files/${req.body.title.split(' ').join('')}.txt`, req.body.details, function(err){});
-        res.redirect('/')
-// console.log(req.body);
+    res.redirect('/')
 })
+//
+app.post('/create',function(req, res)
+{
+fs.rename(`/files/${req.body.previous}`,  `/files/${req.body.new}.txt`,function(err){
+res.redirect("/");
+})
+})
+//
+
+//
 app.listen(3000,function(){
     console.log("Started")
 })
